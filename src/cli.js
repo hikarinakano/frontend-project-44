e8dc38e6aae3e5a83af60f8b3d2c54c4a7161933
Irina <hikarinakano95@gmail.com>
2022-09-15 12:33:23 +0200

    Modified cli.js to work with brain-games.js properly

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,4 @@
 import readlineSync from 'readline-sync';
-function greeting (name) {
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');}
-
-export { greeting };
+	const userName = readlineSync.question('May I have your name? ');
+	console.log('Hi ' + userName + '!');
+export { userName };
