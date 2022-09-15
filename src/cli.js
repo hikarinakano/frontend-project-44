295fba3d8e3765efcce069fcd87f09afbaa1f7bb
Irina <hikarinakano95@gmail.com>
2022-09-15 12:05:45 +0200

    Modified brain-games, package-lock, package.json and modified src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
 import readlineSync from 'readline-sync';
-function greeting () {
+function greeting (name) {
 const userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');}
+
+export { greeting };
