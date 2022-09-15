4b468ae6dd8c93457636679eb8070437da4e494b
Irina <hikarinakano95@gmail.com>
2022-09-15 15:38:36 +0200

    Changed export to default

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
-	const userName = readlineSync.question('May I have your name? ');
-	console.log('Hi ' + userName + '!');
-export { userName };
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hi ${userName}!`);
+export default userName;
