5e804127ce8ff799f1b252843599ca192342118b
Irina <hikarinakano95@gmail.com>
2022-09-22 13:10:57 +0200

    Created brain-even with logic inside

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,27 +1,26 @@
 #!/usr/bin/env node
-import './brain-intro.js';
-import readlineSync from 'readline-sync';
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+import userNameGreeting from '../src/index.js';
 
-function check(counter) {
-  if (counter === 0) {
-    console.log(`Congratulations, ${userName}!`);
-    return;
+function brainEvenCheck(counter) {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    if (counter === 0) {
+      console.log(`Congratulations, ${userNameGreeting}!`);
+      return;
+    }
+    const randomNumber = Math.floor(Math.random() * 20);
+    const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
+  
+    const input = readlineSync.question(`Question: ${randomNumber}\nYour answer:`);
+  
+    if (input !== isEven) {
+      console.log(`'${input}' is wrong answer ;(. Correct answer was '${isEven}'.`);
+      console.log(`Let's try again, ${userNameGreeting}!`);
+      return;
+    }
+    console.log('Correct!');
+    brainEvenCheck(counter - 1);
   }
-  const randomNumber = Math.floor(Math.random() * 20);
-  const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
-
-  const input = readlineSync.question(`Question: ${randomNumber}\nYour answer:`);
-
-  if (input !== isEven) {
-    console.log(`'${input}' is wrong answer ;(. Correct answer was '${isEven}'.`);
-    console.log(`Let's try again, ${userName}!`);
-    return;
-  }
-  console.log('Correct!');
-  check(counter - 1);
-}
-check(3);
+  brainEvenCheck(3);
+  
+  export {brainEvenCheck };
