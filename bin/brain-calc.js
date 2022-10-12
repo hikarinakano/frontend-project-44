485b30d047b6ee733a0516a2d8e52b9776e135df
Irina <hikarinakano95@gmail.com>
2022-10-12 13:59:35 +0200

    Moved greet question above

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,25 +1,24 @@
 #!/usr/bin/env node
 
-import userNameGreeting from '../src/index.js'
 import readlineSync from 'readline-sync';
+import userNameGreeting from '../src/index.js';
 
-
+console.log('What is the result of the expression?');
 function brainCalcCheck(counter) {
-    if (counter === 0) {
-      console.log(`Congratulations, ${userNameGreeting}!`);
-      return;
-    }
-    console.log('What is the result of the expression?');
-    const randomNumber1 = Math.floor(Math.random() * 100);
-    const randomNumber2 = Math.floor(Math.random() * 50);
-    const input = readlineSync.question(`Question: ${randomNumber1} + ${randomNumber2}\nYour answer:`);
-    const whatIsSum = randomNumber1 + randomNumber2;
-    if (whatIsSum - input !== 0) {
-      console.log(`'${input}' is wrong answer ;(. Correct answer was '${whatIsSum}'.`);
-      console.log(`Let's try again, ${userNameGreeting}!`);
-      return;
-    }
-    console.log('Correct!');
-    brainCalcCheck(counter - 1);
+  if (counter === 0) {
+    console.log(`Congratulations, ${userNameGreeting}!`);
+    return;
   }
-  brainCalcCheck(3);
+  const randomNumber1 = Math.floor(Math.random() * 100);
+  const randomNumber2 = Math.floor(Math.random() * 50);
+  const input = readlineSync.question(`Question: ${randomNumber1} + ${randomNumber2}\nYour answer:`);
+  const whatIsSum = randomNumber1 + randomNumber2;
+  if (whatIsSum - input !== 0) {
+    console.log(`'${input}' is wrong answer ;(. Correct answer was '${whatIsSum}'.`);
+    console.log(`Let's try again, ${userNameGreeting}!`);
+    return;
+  }
+  console.log('Correct!');
+  brainCalcCheck(counter - 1);
+}
+brainCalcCheck(3);
