fbf7f6b424b7c28cd734b2e327c82ab438d4a64c
Irina <hikarinakano95@gmail.com>
2022-10-17 11:13:46 +0200

    Minor fixes to code

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -3,24 +3,25 @@ import gameSequence from '../src/index.js';
 
 const greetingQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+function checkPrime(number) {
+  if (number === 0 || number === 1) {
+    return 'no';
+  }
+  if (number === 2) {
+    return 'yes';
+  }
+  const row = [2, 3, 5, 7, 11];
+  for (let i = 0; i <= row.length;) {
+    if (number !== row[i] && number % row[i] === 0) {
+      return 'no';
+    } i += 1;
+  } return 'yes';
+}
+
 function brainLogic() {
   const { randomNumber1 } = randomize();
-  function checkPrime() {
-    if (randomNumber1 === 0 || randomNumber1 === 1) {
-      return 'no';
-    }
-    if (randomNumber1 === 2) {
-      return 'yes';
-    }
-    const row = [2, 3, 5, 7, 11];
-    for (let i = 0; i <= row.length;) {
-      if (randomNumber1 !== row[i] && randomNumber1 % row[i] === 0) {
-        return 'no';
-      } i += 1;
-    } return 'yes';
-  }
   const question = randomNumber1;
-  const rightAnswer = `${checkPrime()}`;
+  const rightAnswer = `${checkPrime(randomNumber1)}`;
   return { question, rightAnswer };
 }
 
