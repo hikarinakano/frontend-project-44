3da9dd57e939a4ea4cfee7824447ae7e61e6491a
Irina <hikarinakano95@gmail.com>
2022-10-17 10:21:24 +0200

    Modified bin file to only launch games

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,37 +1,3 @@
 #!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-import userNameGreeting from '../src/index.js';
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
-
-function brainPrimeCheck(counter) {
-  if (counter === 0) {
-    console.log(`Congratulations, ${userNameGreeting}!`);
-    return;
-  }
-  const randomNumber1 = Math.floor(Math.random() * 100);
-  const fun = function(){
-    if (randomNumber1 === 0 || randomNumber1 === 1 || randomNumber1 % 2 === 0) {
-        return 'no'
-    }
-    let result = [];
-    let row = [3,5,7,11] 
-    for (let i = 0; i <= row.length; i+=1){
-        if (randomNumber1 % row[i] === 0) {
-           result.push('no')
-        } result.push('yes')
-    } if (result.includes('no')){
-        return 'no'
-    } return 'yes';
-  }     
-
-  const input = readlineSync.question(`Question: ${randomNumber1} \nYour answer:`);
-  if (input !== `${fun()}`) {
-    console.log(`'${input}' is wrong answer ;(. Correct answer was '${fun()}'.`);
-    console.log(`Let's try again, ${userNameGreeting}!`);
-    return;
-  }
-  console.log('Correct!');
-  brainPrimeCheck(counter - 1);
-}
-brainPrimeCheck(3);
+import startPrimeGame from "../games/brain-prime.js";
+startPrimeGame();
