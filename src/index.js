3346590906caebbc7acc98e15eada0dcf5d63700
Irina <hikarinakano95@gmail.com>
2022-10-17 10:22:18 +0200

    Changed index.js logic to common for all games

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-function userNameGreeting() {
+function gameSequence(greetingQuestion, brainLogic) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hi ${name}!`);
-  return name;
+  console.log(greetingQuestion)
+
+  for (let round = 1; round <= 3; round += 1) {
+    const { question, rightAnswer } = brainLogic();
+    const userInput = readlineSync.question(`Question: ${question}\nYour answer:`);
+    if (userInput !== rightAnswer) {
+      console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+
+  }
+  console.log(`Congratulations, ${name}!`);
 }
-export default userNameGreeting();
+
+export default gameSequence;
