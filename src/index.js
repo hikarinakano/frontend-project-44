11bf1effddfbf1ac127c18d47a1a330ca934af04
Irina <hikarinakano95@gmail.com>
2022-10-19 17:10:43 +0200

    Changed names and functionality to be more readable

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import readlineSync from 'readline-sync';
 
-function gameSequence(greetingQuestion, brainLogic) {
+function startCommonGameLogic(greetingQuestion, passVariables) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(greetingQuestion);
 
-  for (let round = 1; round <= 3; round += 1) {
-    const { question, rightAnswer } = brainLogic();
-    const userInput = readlineSync.question(`Question: ${question}\nYour answer:`);
+  const gameRoundsAmount = 3;
+
+  for (let round = 1; round <= gameRoundsAmount; round += 1) {
+    const { question, rightAnswer } = passVariables();
+    console.log(`Question: ${question}`);
+    const userInput = readlineSync.question('Your answer:');
     if (userInput !== rightAnswer) {
       console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
@@ -19,4 +22,4 @@ function gameSequence(greetingQuestion, brainLogic) {
   console.log(`Congratulations, ${name}!`);
 }
 
-export default gameSequence;
+export default startCommonGameLogic;
