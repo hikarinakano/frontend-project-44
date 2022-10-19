63ea45f5ede6705721230aeedf66e7d6e548d3d5
Irina <hikarinakano95@gmail.com>
2022-10-19 17:27:28 +0200

    renamed and deleted randomize.js

--- src/randomize.js
+++
@@ -1,7 +0,0 @@
-function randomize() {
-  const randomNumber1 = Math.floor(Math.random() * 30);
-  const randomNumber2 = Math.floor(Math.random() * 20);
-  return { randomNumber1, randomNumber2 };
-}
-
-export default randomize;
