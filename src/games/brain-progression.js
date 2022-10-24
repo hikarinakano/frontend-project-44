fa54c077c277bd31a498990b31f51e9cc0191842
Irina <hikarinakano95@gmail.com>
2022-10-24 10:57:34 +0200

    Small changes and renamings

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,10 +5,10 @@ const gamesDescription = 'What number is missing in the progression?';
 
 const progressionLength = 10;
 
-function createProgression(firstNum, commonDifference) {
+function createProgression(firstNumOfProgression, commonDifference) {
   const result = [];
   for (let i = 1; i <= progressionLength; i += 1) {
-    result.push(firstNum + commonDifference * i);
+    result.push(firstNumOfProgression + commonDifference * i);
   }
   return result;
 }
@@ -17,8 +17,8 @@ function buildRoundsData() {
   const randomNumber = getRandomNumber(1, 30);
   const commonDifference = getRandomNumber(1, 20);
   const newProgression = createProgression(randomNumber, commonDifference);
-  const randomOfProgressionLength = getRandomNumber(0, (newProgression.length - 1));
-  const rightAnswer = String(newProgression[randomOfProgressionLength]);
+  const randomNumOfProgressionLength = getRandomNumber(0, (newProgression.length - 1));
+  const rightAnswer = String(newProgression[randomNumOfProgressionLength]);
   const question = newProgression.join(' ').replace(rightAnswer, '..');
   return { question, rightAnswer };
 }
